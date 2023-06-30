3da0a771ab8038e8e53fa001075bc7d7b60af4f1
paulo barreira <paulobarreira02@gmail.com>
2023-06-30 00:41:45 -0300

    EntregaS6_PetInfo

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -12,14 +12,20 @@ const handleLogin = () => {
     const emailInput = document.querySelector('#input--email')
     const passwordInput = document.querySelector('#input--senha')
     const submitButton = document.querySelector('.login--submit')
+    const spinner = document.querySelector('.spinner')
 
     submitButton.addEventListener('click', async (event) => {
         event.preventDefault()
-        const loginBody = {
-            email: emailInput.value,
-            password: passwordInput.value
-        }
-        const token = loginRequest (loginBody)
+        spinner.classList.remove('hidden')
+        setTimeout(() => {
+            
+            const loginBody = {
+                email: emailInput.value,
+                password: passwordInput.value
+            }
+            spinner.classList.add('hidden')
+            const token = loginRequest (loginBody)
+        }, 2000);
         if(token){
             await userRequest()
         }
